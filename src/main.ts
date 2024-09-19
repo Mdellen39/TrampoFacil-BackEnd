7c2cbf18600964ecfac7529e706d4b914fd9bca4
Mdellen39 <matheus.mrodrigues6@gmail.com>
2024-09-18 21:57:37 -0300

    Inclusão do Swagger e delete na função login
    Na pasta Main, esta a inclusão do swagger, ainda falta colocar o swagger no usuario controller, que ele fica armazenado la, para depois a gente conseguir fazer a documentação.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,23 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     })
   )
-  useContainer(app.select(AppModule),{fallbackOnErrors:true})
+
+
+const config = new DocumentBuilder()
+.setTitle('O Presente projeto é para mostrar um site para a região que facilite a integração entre candidatos e empresas para contratação')
+.setDescription(
+  'A presente API tem como objetivo simular cadastros de possiveis candidatos para as presentes vagas',
+)
+.setVersion('1.0')
+.addTag('usuario')
+.addTag('')
+.addTag('')
+.build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
+
+
+useContainer(app.select(AppModule),{fallbackOnErrors:true})
 }
 bootstrap();
+
