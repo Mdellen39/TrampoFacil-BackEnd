34a32a33ada9c4454858173a34702b1607a469b3
Mdellen39 <matheus.mrodrigues6@gmail.com>
2024-10-16 21:25:31 -0300

    a
    a

--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -1,32 +1,41 @@
-// //classe de modulo do usuário, responsável por administrar todo o modulo de usuário, incluindo controller, DM, e validators, 
-// //tudo o que o modulo de usuário contem, é adinistrado pela classe de módulo
+// // //classe de modulo do usuário, responsável por administrar todo o modulo de usuário, incluindo controller, DM, e validators, 
+// // //tudo o que o modulo de usuário contem, é adinistrado pela classe de módulo
+
+// import { Module } from '@nestjs/common';
+// import { UsuarioController } from './usuario.controller';
+// import { UsuarioService } from './usuario.service'; // Verifique o caminho correto
+// import { emailUnicoValidator } from '../Validacaoemail.validador.ts/email-unico.validator'
+// import { DatabaseModule } from 'src/database/database.module';
+// import { usuarioProviders } from './usuario.providers';
+
+
+// @Module({
+//   imports: [DatabaseModule],
+//   controllers: [UsuarioController],
+//   providers: [...usuarioProviders,
+//     UsuarioService,
+//   ],
+
+// })
+// export class UsuarioModule {}
+
+
+
+
 
 import { Module } from '@nestjs/common';
 import { UsuarioController } from './usuario.controller';
-import { UsuarioService } from './usuario.service'; // Verifique o caminho correto
-import { emailUnicoValidator } from '../Validacaoemail.validador.ts/email-unico.validator'
-import { DatabaseModule } from 'src/database/database.module';
 import { usuarioProviders } from './usuario.providers';
+import { UsuarioService } from './usuario.service';
+import { DatabaseModule } from 'src/database/database.module';
+
 
 @Module({
-  imports: [DatabaseModule],
-  controllers: [UsuarioController],
-  providers: [...usuarioProviders,
-    UsuarioService,
+  imports: [DatabaseModule],  // Importando o módulo do banco de dados
+  controllers: [UsuarioController],  // Definindo o controller do usuário
+  providers: [
+    ...usuarioProviders,  // Injetando os providers do usuário
+    UsuarioService,  // Injetando o serviço do usuário
   ],
-
 })
 export class UsuarioModule {}
-
-
-
-// @Module({  
-//   imports: [DatabaseModule],
-//   controllers: [FilmeController],  
-//   providers: [...filmeProviders,
-//     FilmeService,
-//     ...generoProviders,
-//     GeneroService,
-//   ],
-// })
-// export class FilmeModule {}
